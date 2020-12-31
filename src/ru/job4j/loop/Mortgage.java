7e524418abb788e27123a962249762c6a905a294
ShaDar <alexterentev90@gmail.com>
2020-12-31 12:23:25 +0300

    5.7. Ипотека [#429303]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + amount * (percent / 100.0);
+        double debt = amount;
         while (debt > 0) {
-            debt = debt - salary;
+            debt = debt + debt * (percent / 100.0) - salary;
             year++;
         }
         return year;
