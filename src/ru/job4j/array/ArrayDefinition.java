22f00938b3a479099cb5b2371d23acb2564ce7eb
ShaDar <alexterentev90@gmail.com>
2021-01-02 00:34:42 +0300

    6.0.2. Заполнение массива. [#429225]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("surnames: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("prices: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Вася Васечкин";
+        names[1] = "Петя Петров";
+        names[2] = "Дима Дмитричев";
+        names[3] = "Иван Иванов";
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
